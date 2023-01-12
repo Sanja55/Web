83c09b8ed1f84efd8367059bde2d9bbaab3da866
Sanja <sanja.pistac@gmail.com>
2023-01-12 14:44:50 +0100

    All tasks from DOM exercise completed.

---
+++ DOM-DocumentObjectModel/05-Atributes/main.js
@@ -0,0 +1,25 @@
+/**Create a function that validates the form.
+The function checks each input element that belongs to the form.
+If the element has the required attribute and no data, it should get red borders.*/
+
+function validateTheForm () {
+
+    var form = document.querySelector("form");
+    var inputs = document.querySelectorAll("input");
+
+    for (var i = 0; i < inputs.length; i++) {
+
+        if(inputs[i].hasAttribute("required") && inputs[i].value.length === 0) {
+
+            inputs[i].classList.add("red");
+        
+        } else {
+
+            inputs[i].classList.remove("red");
+        }
+    }
+
+    return inputs;
+}
+
+validateTheForm ();
